d47d9897485ce36f2fa4d90d28cd12bf8caf4941
Michal Zelenák <mychalze@gmail.com>
2022-05-31 00:37:44 +0200

    TrainingItemTemplate implemented
    Cannot fix error

---
+++ MauiApp1/Views/TrainingItemTemplateSelector.cs
@@ -0,0 +1,22 @@
+﻿using MauiApp1.Models;
+using MauiApp1.ViewModels;
+using MauiApp1.Views;
+
+namespace MauiApp1.Views.Selector;
+
+
+public class TrainingItemTemplateSelector:DataTemplateSelector
+{
+    public DataTemplate PauseTemplate { get; set; }
+    public DataTemplate ExerciseTrainingTemplate { get; set; }
+
+    protected override DataTemplate OnSelectTemplate(object item, BindableObject container)
+    {
+        if (item.GetType().Equals(typeof(PauseModel)))
+            return PauseTemplate;
+        return ExerciseTrainingTemplate;
+    }
+}
+
+
+
